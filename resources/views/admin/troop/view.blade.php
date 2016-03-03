55aeb267820c9dfb9584adb9e4211c034dec654c
= <ki4yvc@gmail.com>
2016-03-03 11:06:13 -0500

    made stuff pretty

---
+++ resources/views/admin/troop/view.blade.php
@@ -0,0 +1,26 @@
+@extends('admin.index')
+
+@section('content')
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+      Success!
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <!-- Your Page Content Here -->
+    <p>
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+      SUCCESSS!!
+    </p>
+  </section><!-- /.content -->
+</div><!-- /.content-wrapper -->
+@endsection
